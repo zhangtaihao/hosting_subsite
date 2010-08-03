f5257c02db153ce85874f17635588bc58345e292
Taihao Zhang <zhang.taihao@gmail.com>
2010-08-03 19:25:06 +1000

    Subsite management UI complete.

--- hosting_subsite.js
+++ hosting_subsite.js
@@ -4,3 +4,33 @@
  * @author Taihao Zhang (zhang.taihao@gmail.com)
  * @license GPLv2. See LICENSE.
  */
+
+$(document).ready(function() {
+  // Set up parent window buttons
+  $('.hosting-subsite-button').click(function() {
+    $(document).data('hostingOpenModalFrame', true)
+    var options = {
+      url : '/hosting/js' + $(this).attr('href'),
+      draggable : true,
+      width : 600,
+      height : 150,
+      onSubmit : function(args) {
+        if (args !== null && typeof(args) == 'object') {
+          if (args['action'] == 'refresh') {
+            location.reload();
+          }
+        }
+      }
+    }
+    Drupal.modalFrame.open(options);
+    return false;
+  });
+  
+  // Set up child window buttons
+  $('#hosting-subsite-edit-form a').click(function() {
+    if (parent.Drupal.modalFrame.isOpen) {
+      setTimeout(function() { parent.Drupal.modalFrame.close({}, {}); }, 1);
+      return false;
+    }
+  });
+});
